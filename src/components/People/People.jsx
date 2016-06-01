b638d4fe2bc4a9a8ef963746a032d617aaddfa6c
caraclarke <cara.clarke8@gmail.com>
2016-06-01 14:48:20 -0400

    Rough people done

---
+++ src/components/People/People.jsx
@@ -0,0 +1,43 @@
+var React = require('react');
+
+var People = React.createClass({
+
+  onClick: function(event) {
+    event.stopPropagation();
+
+    // toggle hidden class
+    // responsible for showing/hiding extra information on planets
+    // hidden is in main_style.css sheet in public folder
+    $('#' + this.props.id).toggleClass('hidden');
+  },
+
+  render: function() {
+
+    var propsStyle = {
+      marginTop: 25
+    };
+
+    return (
+      <div className="col-sm-6">
+        <div className="col-md-4">
+          <h3>{this.props.name}</h3>
+          <a href='#' onClick={this.onClick}>Show Details</a>
+        </div>
+
+        <div style={propsStyle} className="col-md-8">
+          <div id={this.props.id} className="hidden compProps">
+            <p><strong>Birth Year::</strong> {this.props.birth_year}</p>
+            <p><strong>Eye Color::</strong> {this.props.eye_color}</p>
+            <p><strong>Gender:</strong> {this.props.gender}</p>
+            <p><strong>Hair Color:</strong> {this.props.hair_color}</p>
+            <p><strong>Height:</strong> {this.props.height}</p>
+            <p><strong>Skin Color:</strong> {this.props.skin_color}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+});
+
+module.exports = People;
